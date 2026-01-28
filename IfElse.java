15773c03dabcfe0874424730acc3e5e80b2d9d13
Ananya Visen <ananyavisen@gmail.com>
2026-01-28 15:28:29 +0530

    If Else in Java

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int n=56749203;
+        int a=3761303, b=732847;
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        if(a>b)
+             System.out.println(a+ " is maximum");
+        else 
+             System.out.println(b + " is maximum");
+    }
+}
