59a1113a3aabff518e183f5ef1dc45cc0d4f02fa
Ananya Visen <ananyavisen@gmail.com>
2026-02-21 20:30:04 +0530

    Calculator using OOPs

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    float add(float a, float b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+ class Test {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        System.out.println("Sum of two integers: " + calc.add(5, 10));
+        System.out.println("Sum of two floats: " + calc.add(5.5f, 2.3f));
+        System.out.println("Sum of three integers: " + calc.add(2, 4, 6));
+    }
+}
