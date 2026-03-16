da0160339c35a3399bf523ba69f76ff0a0f60a8f
Ananya Visen <ananyavisen@gmail.com>
2026-03-16 16:10:31 +0530

    Lambda Expression

---
+++ Lambda.java
@@ -0,0 +1,12 @@
+interface NumericTest  {
+  boolean test(int n);
+    
+}
+
+public class Functional {
+    public static void main(String[] args) {
+        NumericTest isEven = (n) -> (n%2)==0;
+        if(isEven.test(10)) System.out.println("10 is even.");
+        if(!isEven.test(9)) System.out.println("9 is not even.");
+    }
+}
